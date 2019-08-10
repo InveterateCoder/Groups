5855f6d19b23956610f1520cf45d5d9e43873a29
Arthur Grigoryan <inveterate.coder@gmail.com>
2019-08-10 16:27:45 +0300

    something

--- Chat.Web/wwwroot/js/groups.js
+++ Chat.Web/wwwroot/js/groups.js
@@ -5,9 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
         app.on_resize();
     });
 });
+class api_class {
+    register(name, email, password) {
+
+    }
+}
 class reg_panel_class {
     constructor() {
         this.panel = document.getElementById('reg_panel');
+        this.place = 'home';
         this.move();
     }
     move() {
@@ -23,6 +29,76 @@ class reg_panel_class {
         if (isNaN(e.target.value))
             e.target.value = null;
     }
+    clear() {
+        let arr;
+        switch (this.place) {
+            case 'reg':
+                arr = this.panel.children[2].getElementsByTagName('input');
+                for (let i = 0; i < arr.length; i++)
+                    arr[i].value = null;
+                break;
+            case 'sign':
+                arr = this.panel.children[3].getElementsByTagName('input');
+                for (let i = 0; i < arr.length; i++)
+                    arr[i].value = null;
+                break;
+            case 'numb':
+                this.panel.children[4].getElementsByTagName('input')[0].value = null;
+                break;
+        }
+    }
+    btn_hover(el) {
+        el.firstElementChild.style.transform = "scale(1.1,1.1)";
+    }
+    btn_out(el) {
+        el.firstElementChild.style.transform = "scale(1,1)";
+    }
+    btn_down(el) {
+        el.firstElementChild.style.transform = "scale(0.9,0.9)";
+    }
+    btn_up(el) {
+        el.firstElementChild.style.transform = "scale(1.1,1.1)";
+    }
+    goto(place) {
+        switch (place) {
+            case 'home':
+                this.hide();
+                this.panel.children[1].style.display = 'flex';
+                break;
+            case 'reg':
+                this.hide();
+                this.panel.children[2].style.display = 'flex';
+                break;
+            case 'sign':
+                this.hide();
+                this.panel.children[3].style.display = 'flex';
+                break;
+            case 'numb':
+                this.hide();
+                this.panel.children[4].style.display = 'flex';
+                break;
+        }
+        this.place = place;
+    }
+    hide() {
+        switch (this.place) {
+            case 'home':
+                this.panel.children[1].style.display = 'none';
+                break;
+            case 'reg':
+                this.clear();
+                this.panel.children[2].style.display = 'none';
+                break;
+            case 'sign':
+                this.clear();
+                this.panel.children[3].style.display = 'none';
+                break;
+            case 'numb':
+                this.clear();
+                this.panel.children[4].style.display = 'none';
+                break;
+        }
+    }
 }
 class app_class {
     constructor() {
@@ -30,5 +106,11 @@ class app_class {
     }
     on_resize() {
         this.reg_panel.move();
+    }
+    wait() {
+        document.getElementById('wait').style.visibility = 'visible';
+    }
+    resume() {
+        document.getElementById('wait').style.visibility = 'hidden';
     }
 }
